7b4e6def0ce09b6c59330a0bc277fb46ef29c431
DESKTOP-FDSVPNE\Usuario <you@example.com>
2019-11-26 17:46:41 -0300

    avances en routing

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 import classes from './Order.module.css'
 
-const order = () => (
+const order = (props) => (
     <div className={classes.Order}>
-        <p>Ingredients: Salad </p>
+        <p>Ingredients: </p>
+        <p>{JSON.stringify(props.ingredients, null, 2)}</p>
     </div>
 )
 
